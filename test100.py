234c2d2bf6675cf90e7a1450bc0e237a22ec33f3
koya <yasuky123456@gmail.com>
2022-01-05 03:27:19 +0900

    first commit

--- test100.py
+++ test100.py
@@ -22,7 +22,7 @@ import math
 import pydicom
 from PIL import Image
 import re
-#import cv2
+import cv2
 
 
 
@@ -1107,7 +1107,7 @@ if sideradio == 'MR':
     st.subheader('SNRを手計算でしている方必見!')
 
     
-    uploaded_files_1 = st.file_uploader("★DATA_1を選択してね！", accept_multiple_files=True)
+    uploaded_files_1 = st.file_uploader("★DATA_1を選択！", accept_multiple_files=True)
     datas_1 = []
     for uploaded_file_1 in uploaded_files_1:
         img_1 = pydicom.dcmread(uploaded_file_1)
@@ -1120,7 +1120,7 @@ if sideradio == 'MR':
     #st.write(datas_1)
     #st.write(df_1)
 
-    uploaded_files_2 = st.file_uploader("★DATA_2を選択してね！", accept_multiple_files=True)
+    uploaded_files_2 = st.file_uploader("★DATA_2を選択！", accept_multiple_files=True)
     datas_2 = []
     for uploaded_file_2 in uploaded_files_2:
         img_2 = pydicom.dcmread(uploaded_file_2)
@@ -1137,11 +1137,320 @@ if sideradio == 'MR':
     z = int(st.number_input('★ROIのピクセル数を入力．imageJ等で調査'))
     #'入力された銘柄名:', Security_code
     if st.button('ROI設定とSNR結果一覧'):
-        st.write('ROI画像')
+
+        z_pixcel =int(math.sqrt(z))
+            #st.write("imageJのROIピクセル数入力:"+str(z))
+            #st.write("ROI設定:画像の中心とした正方形" + str(z_pixcel) + "*"+ str(z_pixcel) + "領域")
+            #ROI設定
+        i = int(z_pixcel / 2)
+            #中心座標127
+        I = 127 - i
+        II = 127 + i
+
         fig, ax = plt.subplots(figsize=(1,1))
-        ax.imshow(pixels_1,cmap='gray')
+        #ax.imshow(pixels_1,cmap='gray')
+        #ax.set_axis_off()
+        #st.pyplot(fig)
+        img_ROI = cv2.rectangle(pixels_1, (I,I), (II, II), (0, 0, 0), thickness=-1)
+        ax.imshow(img_ROI,cmap='gray')
         ax.set_axis_off()
         st.pyplot(fig)
+
+        #dataの結合
+        df_1 = pd.concat(datas_1,axis=0)
+        df_1 = df_1.astype("float64")
+        df_2 = pd.concat(datas_2,axis=0)
+        df_2 = df_2.astype("float64")
+
+
+
+
+        #インデックス設定
+        df_1 = df_1.reset_index(drop=True)
+        df_2 = df_2.reset_index(drop=True)
+
+
+        DF = pd.concat([df_1,df_2])
+        
+        DF_sort = DF.sort_index(axis=0)
+
+        #st.write("df1+2")
+        #st.write(DF_sort)
+
+        #読み込み全データの対応する組の各行の平均
+        EPI_Ave_Signal = DF_sort.groupby(level=0).mean()
+        #st.write("EPI_Ave_Signalの結果")
+        #st.write(EPI_Ave_Signal)
+        #差分
+        EPI_subtra = df_1 - df_2
+
+        #st.write("subtrea結果")
+        #st.write(EPI_subtra)
+
+
+        #範囲変数作成
+        num = 256
+        for i in range(51):
+            code = 'row{} = {}'.format(i, i * num) 
+            exec(code)
+        
+        
+        #noiseSD計算 範囲とROI領域とlist化 に対して標準偏差
+        A_noiseSD =np.std(EPI_subtra.iloc[0:row1, :].iloc[I:II,I:II].to_numpy().tolist())
+        B_noiseSD =np.std(EPI_subtra.iloc[row1:row2, :].iloc[I:II,I:II].to_numpy().tolist())
+        C_noiseSD =np.std(EPI_subtra.iloc[row2:row3, :].iloc[I:II,I:II].to_numpy().tolist())
+        D_noiseSD =np.std(EPI_subtra.iloc[row3:row4, :].iloc[I:II,I:II].to_numpy().tolist())
+        E_noiseSD =np.std(EPI_subtra.iloc[row4:row5, :].iloc[I:II,I:II].to_numpy().tolist())
+        F_noiseSD =np.std(EPI_subtra.iloc[row5:row6, :].iloc[I:II,I:II].to_numpy().tolist())
+        G_noiseSD =np.std(EPI_subtra.iloc[row6:row7, :].iloc[I:II,I:II].to_numpy().tolist())
+        H_noiseSD =np.std(EPI_subtra.iloc[row7:row8, :].iloc[I:II,I:II].to_numpy().tolist())
+        I_noiseSD =np.std(EPI_subtra.iloc[row8:row9, :].iloc[I:II,I:II].to_numpy().tolist())
+        J_noiseSD =np.std(EPI_subtra.iloc[row9:row10, :].iloc[I:II,I:II].to_numpy().tolist())
+        K_noiseSD =np.std(EPI_subtra.iloc[row10:row11, :].iloc[I:II,I:II].to_numpy().tolist())
+        L_noiseSD =np.std(EPI_subtra.iloc[row11:row12, :].iloc[I:II,I:II].to_numpy().tolist())
+        M_noiseSD =np.std(EPI_subtra.iloc[row12:row13, :].iloc[I:II,I:II].to_numpy().tolist())
+        N_noiseSD =np.std(EPI_subtra.iloc[row13:row14, :].iloc[I:II,I:II].to_numpy().tolist())
+        O_noiseSD =np.std(EPI_subtra.iloc[row14:row15, :].iloc[I:II,I:II].to_numpy().tolist())
+        P_noiseSD =np.std(EPI_subtra.iloc[row15:row16, :].iloc[I:II,I:II].to_numpy().tolist())
+        Q_noiseSD =np.std(EPI_subtra.iloc[row16:row17, :].iloc[I:II,I:II].to_numpy().tolist())
+        R_noiseSD =np.std(EPI_subtra.iloc[row17:row18, :].iloc[I:II,I:II].to_numpy().tolist())
+        S_noiseSD =np.std(EPI_subtra.iloc[row18:row19, :].iloc[I:II,I:II].to_numpy().tolist())
+        T_noiseSD =np.std(EPI_subtra.iloc[row19:row20, :].iloc[I:II,I:II].to_numpy().tolist())
+        U_noiseSD =np.std(EPI_subtra.iloc[row20:row21, :].iloc[I:II,I:II].to_numpy().tolist())
+        V_noiseSD =np.std(EPI_subtra.iloc[row21:row22, :].iloc[I:II,I:II].to_numpy().tolist())
+        W_noiseSD =np.std(EPI_subtra.iloc[row22:row23, :].iloc[I:II,I:II].to_numpy().tolist())
+        X_noiseSD =np.std(EPI_subtra.iloc[row23:row24, :].iloc[I:II,I:II].to_numpy().tolist())
+        Y_noiseSD =np.std(EPI_subtra.iloc[row24:row25, :].iloc[I:II,I:II].to_numpy().tolist())
+        Z_noiseSD =np.std(EPI_subtra.iloc[row25:row26, :].iloc[I:II,I:II].to_numpy().tolist())
+        AA_noiseSD =np.std(EPI_subtra.iloc[row26:row27, :].iloc[I:II,I:II].to_numpy().tolist())
+        BB_noiseSD =np.std(EPI_subtra.iloc[row27:row28, :].iloc[I:II,I:II].to_numpy().tolist())
+        CC_noiseSD =np.std(EPI_subtra.iloc[row28:row29, :].iloc[I:II,I:II].to_numpy().tolist())
+        DD_noiseSD =np.std(EPI_subtra.iloc[row29:row30, :].iloc[I:II,I:II].to_numpy().tolist())
+        EE_noiseSD =np.std(EPI_subtra.iloc[row30:row31, :].iloc[I:II,I:II].to_numpy().tolist())
+        FF_noiseSD =np.std(EPI_subtra.iloc[row31:row32, :].iloc[I:II,I:II].to_numpy().tolist())
+        GG_noiseSD =np.std(EPI_subtra.iloc[row32:row33, :].iloc[I:II,I:II].to_numpy().tolist())
+        HH_noiseSD =np.std(EPI_subtra.iloc[row33:row34, :].iloc[I:II,I:II].to_numpy().tolist())
+        II_noiseSD =np.std(EPI_subtra.iloc[row34:row35, :].iloc[I:II,I:II].to_numpy().tolist())
+        JJ_noiseSD =np.std(EPI_subtra.iloc[row35:row36, :].iloc[I:II,I:II].to_numpy().tolist())
+        KK_noiseSD =np.std(EPI_subtra.iloc[row36:row37, :].iloc[I:II,I:II].to_numpy().tolist())
+        LL_noiseSD =np.std(EPI_subtra.iloc[row37:row38, :].iloc[I:II,I:II].to_numpy().tolist())
+        MM_noiseSD =np.std(EPI_subtra.iloc[row38:row39, :].iloc[I:II,I:II].to_numpy().tolist())
+        NN_noiseSD =np.std(EPI_subtra.iloc[row39:row40, :].iloc[I:II,I:II].to_numpy().tolist())
+        OO_noiseSD =np.std(EPI_subtra.iloc[row40:row41, :].iloc[I:II,I:II].to_numpy().tolist())
+        PP_noiseSD =np.std(EPI_subtra.iloc[row41:row42, :].iloc[I:II,I:II].to_numpy().tolist())
+        QQ_noiseSD =np.std(EPI_subtra.iloc[row42:row43, :].iloc[I:II,I:II].to_numpy().tolist())
+        RR_noiseSD =np.std(EPI_subtra.iloc[row43:row44, :].iloc[I:II,I:II].to_numpy().tolist())
+        SS_noiseSD =np.std(EPI_subtra.iloc[row44:row45, :].iloc[I:II,I:II].to_numpy().tolist())
+        TT_noiseSD =np.std(EPI_subtra.iloc[row45:row46, :].iloc[I:II,I:II].to_numpy().tolist())
+        UU_noiseSD =np.std(EPI_subtra.iloc[row46:row47, :].iloc[I:II,I:II].to_numpy().tolist())
+        VV_noiseSD =np.std(EPI_subtra.iloc[row47:row48, :].iloc[I:II,I:II].to_numpy().tolist())
+        WW_noiseSD =np.std(EPI_subtra.iloc[row48:row49, :].iloc[I:II,I:II].to_numpy().tolist())
+        XX_noiseSD =np.std(EPI_subtra.iloc[row49:row50, :].iloc[I:II,I:II].to_numpy().tolist())
+
+
+
+
+
+        #st.write(A_noiseSD)
+    
+        #平均計算　範囲とROI領域とlist化 に対して平均
+        a_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[0:row1, :].iloc[I:II,I:II].to_numpy().tolist())
+        b_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row1:row2, :].iloc[I:II,I:II].to_numpy().tolist())
+        c_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row2:row3, :].iloc[I:II,I:II].to_numpy().tolist())
+        d_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row3:row4, :].iloc[I:II,I:II].to_numpy().tolist())
+        e_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row4:row5, :].iloc[I:II,I:II].to_numpy().tolist())
+        f_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row5:row6, :].iloc[I:II,I:II].to_numpy().tolist())
+        g_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row6:row7, :].iloc[I:II,I:II].to_numpy().tolist())
+        h_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row7:row8, :].iloc[I:II,I:II].to_numpy().tolist())
+        i_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row8:row9, :].iloc[I:II,I:II].to_numpy().tolist())
+        j_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row9:row10, :].iloc[I:II,I:II].to_numpy().tolist())
+        k_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row10:row11, :].iloc[I:II,I:II].to_numpy().tolist())
+        l_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row11:row12, :].iloc[I:II,I:II].to_numpy().tolist())
+        m_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row12:row13, :].iloc[I:II,I:II].to_numpy().tolist())
+        n_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row13:row14, :].iloc[I:II,I:II].to_numpy().tolist())
+        o_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row14:row15, :].iloc[I:II,I:II].to_numpy().tolist())
+        p_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row15:row16, :].iloc[I:II,I:II].to_numpy().tolist())
+        q_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row16:row17, :].iloc[I:II,I:II].to_numpy().tolist())
+        r_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row17:row18, :].iloc[I:II,I:II].to_numpy().tolist())
+        s_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row18:row19, :].iloc[I:II,I:II].to_numpy().tolist())
+        t_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row19:row20, :].iloc[I:II,I:II].to_numpy().tolist())
+        u_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row20:row21, :].iloc[I:II,I:II].to_numpy().tolist())
+        v_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row21:row22, :].iloc[I:II,I:II].to_numpy().tolist())
+        w_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row22:row23, :].iloc[I:II,I:II].to_numpy().tolist())
+        x_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row23:row24, :].iloc[I:II,I:II].to_numpy().tolist())
+        y_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row24:row25, :].iloc[I:II,I:II].to_numpy().tolist())
+        z_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row25:row26, :].iloc[I:II,I:II].to_numpy().tolist())
+        aa_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row26:row27, :].iloc[I:II,I:II].to_numpy().tolist())
+        bb_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row27:row28, :].iloc[I:II,I:II].to_numpy().tolist())
+        cc_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row28:row29, :].iloc[I:II,I:II].to_numpy().tolist())
+        dd_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row29:row30, :].iloc[I:II,I:II].to_numpy().tolist())
+        ee_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row30:row31, :].iloc[I:II,I:II].to_numpy().tolist())
+        ff_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row31:row32, :].iloc[I:II,I:II].to_numpy().tolist())
+        gg_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row32:row33, :].iloc[I:II,I:II].to_numpy().tolist())
+        hh_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row33:row34, :].iloc[I:II,I:II].to_numpy().tolist())
+        ii_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row34:row35, :].iloc[I:II,I:II].to_numpy().tolist())
+        jj_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row35:row36, :].iloc[I:II,I:II].to_numpy().tolist())
+        kk_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row36:row37, :].iloc[I:II,I:II].to_numpy().tolist())
+        ll_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row37:row38, :].iloc[I:II,I:II].to_numpy().tolist())
+        mm_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row38:row39, :].iloc[I:II,I:II].to_numpy().tolist())
+        nn_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row39:row40, :].iloc[I:II,I:II].to_numpy().tolist())
+        oo_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row40:row41, :].iloc[I:II,I:II].to_numpy().tolist())
+        pp_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row41:row42, :].iloc[I:II,I:II].to_numpy().tolist())
+        qq_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row42:row43, :].iloc[I:II,I:II].to_numpy().tolist())
+        rr_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row43:row44, :].iloc[I:II,I:II].to_numpy().tolist())
+        ss_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row44:row45, :].iloc[I:II,I:II].to_numpy().tolist())
+        tt_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row45:row46, :].iloc[I:II,I:II].to_numpy().tolist())
+        uu_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row46:row47, :].iloc[I:II,I:II].to_numpy().tolist())
+        vv_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row47:row48, :].iloc[I:II,I:II].to_numpy().tolist())
+        ww_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row48:row49, :].iloc[I:II,I:II].to_numpy().tolist())
+        xx_EPI_Ave_Signal =np.mean(EPI_Ave_Signal.iloc[row49:row50, :].iloc[I:II,I:II].to_numpy().tolist())
+        
+        #st.write(a_EPI_Ave_Signal)
+
+
+
+        #SNR
+        SNR_0 = a_EPI_Ave_Signal / (A_noiseSD /math.sqrt(2))
+        SNR_1 = b_EPI_Ave_Signal / (B_noiseSD /math.sqrt(2))
+        SNR_2 = c_EPI_Ave_Signal / (C_noiseSD /math.sqrt(2))
+        SNR_3 = d_EPI_Ave_Signal / (D_noiseSD /math.sqrt(2))
+        SNR_4 = e_EPI_Ave_Signal / (E_noiseSD /math.sqrt(2))
+        SNR_5 = f_EPI_Ave_Signal / (F_noiseSD /math.sqrt(2))
+        SNR_6 = g_EPI_Ave_Signal / (G_noiseSD /math.sqrt(2))
+        SNR_7 = h_EPI_Ave_Signal / (H_noiseSD /math.sqrt(2))
+        SNR_8 = i_EPI_Ave_Signal / (I_noiseSD /math.sqrt(2))
+        SNR_9 = j_EPI_Ave_Signal / (J_noiseSD /math.sqrt(2))
+        SNR_10 = k_EPI_Ave_Signal / (K_noiseSD /math.sqrt(2))
+        SNR_11 = l_EPI_Ave_Signal / (L_noiseSD /math.sqrt(2))
+        SNR_12 = m_EPI_Ave_Signal / (M_noiseSD /math.sqrt(2))
+        SNR_13 = n_EPI_Ave_Signal / (N_noiseSD /math.sqrt(2))
+        SNR_14 = o_EPI_Ave_Signal / (O_noiseSD /math.sqrt(2))
+        SNR_15 = p_EPI_Ave_Signal / (P_noiseSD /math.sqrt(2))
+        SNR_16 = q_EPI_Ave_Signal / (Q_noiseSD /math.sqrt(2))
+        SNR_17 = r_EPI_Ave_Signal / (R_noiseSD /math.sqrt(2))
+        SNR_18 = s_EPI_Ave_Signal / (S_noiseSD /math.sqrt(2))
+        SNR_19 = t_EPI_Ave_Signal / (T_noiseSD /math.sqrt(2))
+        SNR_20 = u_EPI_Ave_Signal / (U_noiseSD /math.sqrt(2))
+        SNR_21 = v_EPI_Ave_Signal / (V_noiseSD /math.sqrt(2))
+        SNR_22 = w_EPI_Ave_Signal / (W_noiseSD /math.sqrt(2))
+        SNR_23 = x_EPI_Ave_Signal / (X_noiseSD /math.sqrt(2))
+        SNR_24 = y_EPI_Ave_Signal / (Y_noiseSD /math.sqrt(2))
+        SNR_25 = z_EPI_Ave_Signal / (Z_noiseSD /math.sqrt(2))
+        SNR_26 = aa_EPI_Ave_Signal / (AA_noiseSD /math.sqrt(2))
+        SNR_27 = bb_EPI_Ave_Signal / (BB_noiseSD /math.sqrt(2))
+        SNR_28 = cc_EPI_Ave_Signal / (CC_noiseSD /math.sqrt(2))
+        SNR_29 = dd_EPI_Ave_Signal / (DD_noiseSD /math.sqrt(2))
+        SNR_30 = ee_EPI_Ave_Signal / (EE_noiseSD /math.sqrt(2))
+        SNR_31 = ff_EPI_Ave_Signal / (FF_noiseSD /math.sqrt(2))
+        SNR_32 = gg_EPI_Ave_Signal / (GG_noiseSD /math.sqrt(2))
+        SNR_33 = hh_EPI_Ave_Signal / (HH_noiseSD /math.sqrt(2))
+        SNR_34 = ii_EPI_Ave_Signal / (II_noiseSD /math.sqrt(2))
+        SNR_35 = jj_EPI_Ave_Signal / (JJ_noiseSD /math.sqrt(2))
+        SNR_36 = kk_EPI_Ave_Signal / (KK_noiseSD /math.sqrt(2))
+        SNR_37 = ll_EPI_Ave_Signal / (LL_noiseSD /math.sqrt(2))
+        SNR_38 = mm_EPI_Ave_Signal / (MM_noiseSD /math.sqrt(2))
+        SNR_39 = nn_EPI_Ave_Signal / (NN_noiseSD /math.sqrt(2))
+        SNR_40 = oo_EPI_Ave_Signal / (OO_noiseSD /math.sqrt(2))
+        SNR_41 = pp_EPI_Ave_Signal / (PP_noiseSD /math.sqrt(2))
+        SNR_42 = qq_EPI_Ave_Signal / (QQ_noiseSD /math.sqrt(2))
+        SNR_43 = rr_EPI_Ave_Signal / (RR_noiseSD /math.sqrt(2))
+        SNR_44 = ss_EPI_Ave_Signal / (SS_noiseSD /math.sqrt(2))
+        SNR_45 = tt_EPI_Ave_Signal / (TT_noiseSD /math.sqrt(2))
+        SNR_46 = uu_EPI_Ave_Signal / (UU_noiseSD /math.sqrt(2))
+        SNR_47 = vv_EPI_Ave_Signal / (VV_noiseSD /math.sqrt(2))
+        SNR_48 = ww_EPI_Ave_Signal / (WW_noiseSD /math.sqrt(2))
+        SNR_49 = xx_EPI_Ave_Signal / (XX_noiseSD /math.sqrt(2))
+
+
+
+
+
+
+
+        SNR_list = [SNR_0,SNR_1,SNR_2,SNR_3,SNR_4,SNR_5,SNR_6,SNR_7,SNR_8,SNR_9,
+                    SNR_10,SNR_11,SNR_12,SNR_13,SNR_14,SNR_15,SNR_16,SNR_17,SNR_18,SNR_19,
+                    SNR_20,SNR_21,SNR_22,SNR_23,SNR_24,SNR_25,SNR_26,SNR_27,SNR_28,SNR_29,
+                    SNR_30,SNR_31,SNR_32,SNR_33,SNR_34,SNR_35,SNR_36,SNR_37,SNR_38,SNR_39,
+                    SNR_40,SNR_41,SNR_42,SNR_43,SNR_44,SNR_45,SNR_46,SNR_47,SNR_48,SNR_49
+                    ]
+        
+        
+        SNR_df = pd.DataFrame(SNR_list, columns = ['SNR'])
+        SNR_dropna = SNR_df.dropna()
+       
+        
+        
+        #SNRの平均
+        SNR_mean= SNR_dropna.mean()
+        SNR_mean_list =[SNR_mean]
+
+        SNR_mean_list_df = pd.DataFrame(SNR_mean_list,index=['平均SNR'])
+
+
+        #displayの見せ方
+        #st.write(SNR_0)
+        st.title('結果')
+        #st.write(SNR_df)
+        #st.write(SNR_dropna)
+        #st.table(SNR_dropna)
+
+        #st.write(SNR_mean)
+
+        #st.table(SNR_mean_list_df)
+    
+
+        #data_num = len(files_1)
+        #lists = list(range(data_num))
+        #SNR_dropna['a'] = lists
+        #st.write(SNR_dropna)
+
+
+        #SNR_df = SNR_dropna.reset_index(drop=True)
+        #st.write(SNR_df)
+
+        SNR_df = SNR_dropna.reset_index()
+        #st.write(SNR_df)
+
+        SNR_meann = pd.DataFrame(SNR_mean_list, index=[9000])
+
+        SNR_mean_df = SNR_meann.reset_index()
+        #st.write(SNR_mean_df)
+        df_con = pd.concat([SNR_df, SNR_mean_df])
+        #st.write(df_con)
+
+        dfcon_reset = df_con.reset_index(drop=True)
+        #st.write(dfcon_reset)
+
+
+        gg = dfcon_reset.astype(str)
+
+        df_new = gg.replace('9000','平均SNR')
+        #st.write(df_new)
+
+        #datafream_reset は文字列
+        reset=df_new.set_index('index')
+        #st.table(reset)
+
+        reset = reset['SNR'].astype(float)
+        st.table(reset) 
+
+
+        @st.cache
+        def convert_df(df):
+            # IMPORTANT: Cache the conversion to prevent computation on every rerun
+            return df.to_csv().encode('utf-8-sig')
+
+        csv = convert_df(reset)
+        #csv = convert_df(SNR_mean_list_df)
+
+        st.download_button(
+            label="CSV出力",
+            data=csv,
+            file_name='SNR.csv',
+            mime='csv',
+        )
+        
+       
 
 
 
